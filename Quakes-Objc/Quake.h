3a2a49ab910b11b759f2a85c6dbbe3d5de025b11
Kevin Stewart <kistewart419@gmail.com>
2020-07-27 22:21:26 -0400

    Added the interface for a Quake

--- Quakes-Objc/Quake.h
+++ Quakes-Objc/Quake.h
@@ -12,6 +12,21 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Quake : NSObject
 
+// NSString, NSArray, NSDictionary mark with copy when making the properties!
+
+- (instancetype)initWithMagnitude:(double)magnitude
+                            place:(NSString *)place
+                             time:(NSDate *)time
+                         latitude:(double)latitude
+                        longitude:(double)longitude;
+
+// Read only makes the property immutable
+@property (nonatomic, readonly) double magnitude;
+@property (nonatomic, readonly, copy) NSString *place;
+@property (nonatomic, readonly) NSDate *time;
+@property (nonatomic, readonly) double *latitude;
+@property (nonatomic, readonly) double *longitude;
+
 @end
 
 NS_ASSUME_NONNULL_END
