96d00ee396e77b0633fabe9b02a993af06c6b5da
Kevin Stewart <kistewart419@gmail.com>
2020-07-27 21:37:52 -0400

    Added first responder file (sub class NSObject)

---
+++ Quakes-Objc/FirstResponder.m
@@ -0,0 +1,13 @@
+//
+//  FirstResponder.m
+//  Quakes-Objc
+//
+//  Created by Kevin Stewart on 7/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "FirstResponder.h"
+
+@implementation FirstResponder
+
+@end
